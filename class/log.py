dd0d85c081d77b745fbc035e83f60e25d95236b8
João Gabriel <contato_joaogabriel@outlook.com>
2020-04-22 00:33:30 -0300

    Classe: Log - concluída

---
+++ class/log.py
@@ -0,0 +1,22 @@
+import os;
+import platform;
+from log.log_funções import *;
+
+class Dispositivo:
+
+    def __init__(self):
+        self.__nomemaquina = platform.uname().node;
+        self.__sistemaoperacional = platform.uname().system;
+        self.__loginmaquina = os.environ['Username'];
+
+    @property
+    def getNomeMaquina(self):
+        return self.__nomemaquina;
+
+    @property
+    def getSistemaOperacional(self):
+        return self.__sistemaoperacional;
+
+    @property
+    def getLoginMaquina(self):
+        return self.__loginmaquina;
