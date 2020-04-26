e6c28bf32b0cb8d76d47a6e908233204944a98d4
João Gabriel <contato_joaogabriel@outlook.com>
2020-04-26 01:42:07 -0300

    Requisição e tratamento de filtros concluida...

--- application/requisicao_filtros.py
+++ application/requisicao_filtros.py
@@ -1,19 +1,32 @@
-def Requisita_Filtros():
+from classes.comandosql import *;
+from log_paste.log_funções import PrintarLogFunção;
+
+EndereçoFunção = "disparador_email.application.requisicao_filtros.py"
+InformacaoFiltros = """
+1. Nome exato           (Nome igual ao nome informado)
+2. Sobrenome exato      (Sobrenome igual ao sobrenome informado)
+3. Email exato          (Email igual ao email informado)
+4. Data de nascimento   (Data de nascimento igual a data de nascimento informada)
+"""
+
+def Requisita_Filtros(Filtros_Disponiveis):
     Continua:        bool=False;
     Confirmacao:     str;
     Entrada:         int;
     Filtros = [];
 
+    print(InformacaoFiltros);
     while Continua != True:
+
         try:
-            Entrada = int(input(f"Por favor, insira o filtro (entre {min(Filtros_Disponiveis)} e {max(Filtros_Disponiveis)}): "));
+            Entrada = int(input(f"Por favor, insira o filtro (entre {min(Filtros_Disponiveis) + 1} e {max(Filtros_Disponiveis) + 1}): "));
             if Entrada not in Filtros:
-                if Entrada in Filtros_Disponiveis:
-                    Filtros.append(Entrada);
+                if Entrada - 1 in Filtros_Disponiveis:
+                    Filtros.append(Entrada - 1);
                     print(f"Filtro {Entrada} adicionado com sucesso.");
 
                 else:
-                    print(f"Esse filtro não se encontra disponível! Por favor, selecione um desses filros: {Filtros_Disponiveis}");
+                    print(f"Esse filtro não se encontra disponível! Por favor, selecione entre {min(Filtros_Disponiveis) + 1} e {max(Filtros_Disponiveis) + 1}");
 
             else:
                 print(f"Você já inseriu esse filtro... Filtros solicitados: {Filtros}");
@@ -37,8 +50,35 @@ def Requisita_Filtros():
     Confirmacao = None;
     Entrada = None;
 
+    PrintarLogFunção(True, EndereçoFunção, "Função Requisita_Filtros");
+    print(Filtros);
     return Filtros;
 
+def Trata_Filtros():
+    SQL_info = ComandoSQL();
+    Filtros_Disponiveis = range(0, len(SQL_info.getComandoSQL));
+    SelecaoSQL: str = f"SELECT * FROM {SQL_info.getTabela}";
 
-Filtros_Disponiveis = [1,2,3,4,5,6];
-print(Requisita_Filtros());
+    FiltrosRequisitados = Requisita_Filtros(Filtros_Disponiveis);
+
+    Entrada:    str = "";
+
+    for Filtro in FiltrosRequisitados:
+        while Entrada == "":
+            Entrada = str(input(f"Por favor, insira o valor do filtro {Filtro + 1}!"));
+
+            if Entrada == "" or len(Entrada) == 0:
+                print(f"O valor '{Entrada}' é inválido para a operação");
+            else:
+                if "WHERE" in SelecaoSQL:
+                    SelecaoSQL += " AND";
+                else:
+                    SelecaoSQL += " WHERE";
+
+                SelecaoSQL += SQL_info.GeraSQL(Entrada, Filtro);
+
+        Entrada = "";
+
+    PrintarLogFunção(True, EndereçoFunção, "Função Trata_Filtros");
+    SQL_info = None;
+    return SelecaoSQL;
--- classes/comandosql.py
+++ classes/comandosql.py
@@ -1,8 +1,30 @@
 class ComandoSQL:
 
     def __init__(self):
+        self.Parametro = "";
         self.__tabela = "tab_pessoas";
+        self.__comandosql = [
+            f" nome =",
+            f" sobrenome =",
+            f" email =",
+            f" data_nascimento ="
+            ];
 
+    @property
+    def getTabela(self):
+        return self.__tabela;
 
-    def GerarSQL(self, Filtro):
-        return self.ComandosSQL[Filtro];
+    @property
+    def getComandoSQL(self):
+        return self.__comandosql;
+
+    @getTabela.setter
+    def setTabela(self, Tabela):
+        self.__tabela = Tabela;
+
+    @getComandoSQL.setter
+    def setComandoSQL(self, ComandoSQL):
+        self.__comandosql = ComandoSQL;
+
+    def GeraSQL(self, Parametro, Filtro):
+        return f"{self.getComandoSQL[Filtro]} '{Parametro}'";
